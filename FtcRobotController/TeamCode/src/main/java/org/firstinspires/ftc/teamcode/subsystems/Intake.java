2e137ae0bb266ee4fca4b83bad24004dfa3ea581
chenwen051116 <TavenChen@outlook.com>
2025-10-15 16:29:12 -0700

    Update Intake.java

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import android.hardware.Sensor;
-
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -13,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class Intake extends SubsystemBase {
     private final DcMotor intake, transfer;
     private final DistanceSensor transferBreakBeam;
-    public boolean ballAtTransfer = false;
+    private IntakeTransferState intakeCurrentState = IntakeTransferState.Intake_Steady;
 
     public Intake(HardwareMap hardwareMap) {      //Constructor,新建对象时需要
         intake = hardwareMap.get(DcMotor.class, "intake");
@@ -23,7 +21,7 @@ public class Intake extends SubsystemBase {
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         transfer.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-       intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public double getDist(){
@@ -37,12 +35,27 @@ public class Intake extends SubsystemBase {
     public void setTransferPower(double power) {
         transfer.setPower(power);
     }
+    public enum IntakeTransferState {
+        Suck_In(1,0),
+        Split_Out(-0.7, -1),
+        Send_It_Up(1,1),
+        Intake_Steady(0,0);
+        private final double intakePower;
+        private final double transferPower;
+        IntakeTransferState(double InPower, double TrPower) {
+            this.intakePower = InPower;
+            this.transferPower = TrPower;
+        }
+    }
+    public void setIntakeState(IntakeTransferState intakeTransferState) {
+        intakeCurrentState = intakeTransferState;
+        intake.setPower(intakeCurrentState.intakePower);
+        transfer.setPower(intakeCurrentState.transferPower);
+
+    }
+
     @Override
     public void periodic() { // FTC 0.001s cycle
-//        if (getDist() < 100) {
-//            ballAtTransfer = true;
-//        }else{
-//            ballAtTransfer = false;
-//        }
+
     }
 }
