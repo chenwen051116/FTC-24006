99cc8ae6325ce715ae574210aebb1e180471355f
chenwen051116 <TavenChen@outlook.com>
2025-10-17 21:42:39 -0700

    Update TestAuto.java

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAuto.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAuto.java
@@ -75,14 +75,17 @@ public class TestAuto extends OpMode {
                         firstshooting = true;
                     }
                     else{
-                        if(timer.getElapsedTimeSeconds()> 6){
+                        if(timer.getElapsedTimeSeconds()> 5.6){
                             shooter.setShooterStatus(Shooter.ShooterStatus.Stop);
                             setPathState(2);
+                        }
+                        if(timer.getElapsedTimeSeconds()>2.5&&timer.getElapsedTimeSeconds()<3){
+                            intake.setIntakeState(Intake.IntakeTransferState.Intake_Steady);
                         }
                         if(timer.getElapsedTimeSeconds()>3&&timer.getElapsedTimeSeconds()<3.5){
                             intake.setIntakeState(Intake.IntakeTransferState.Split_Out);
                         }
-                        if(timer.getElapsedTimeSeconds()>3.5&&timer.getElapsedTimeSeconds()<4.5){
+                        if(timer.getElapsedTimeSeconds()>3.5&&timer.getElapsedTimeSeconds()<5){
                             intake.setIntakeState(Intake.IntakeTransferState.Suck_In);
                         }
                         if(timer.getElapsedTimeSeconds()>5.5){
