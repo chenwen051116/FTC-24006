ec01d3c43d741becaa733ed2ef7387066874cef7
chenwen051116 <TavenChen@outlook.com>
2025-12-02 19:56:05 -0800

    Update Drivetrain.java

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -24,12 +24,12 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 public class Drivetrain extends SubsystemBase {
 
     //declare motors.. 声明，赋值...
-    private final DcMotor frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
+    //private final DcMotor frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
 
 
     private MecanumDrive drive;
 
-    //public Follower follower;
+    public Follower follower;
 
     public GoBildaPinpointDriver pin;
 
@@ -37,72 +37,74 @@ public class Drivetrain extends SubsystemBase {
 
     public Pose rednearAimPos = new Pose(418.15,-753,0);
     public Pose aimPos = bluenearAimPos;
+
     //servos
 
     public Drivetrain(HardwareMap hardwareMap) {      //Constructor,新建对象时需要
-        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeft");
-        backRightMotor = hardwareMap.get(DcMotor.class, "backRight");
+//        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
+//        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
+//        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeft");
+//        backRightMotor = hardwareMap.get(DcMotor.class, "backRight");
        // drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
 
-//        follower = Constants.createFollower(hardwareMap);
-//        PanelsConfigurables.INSTANCE.refreshClass(this);
-//        follower.startTeleopDrive();
-//        follower.update();
-//        follower.setStartingPose(new Pose(0,0,0));
-        pin = hardwareMap.get(GoBildaPinpointDriver.class,"pinpoint");
-        pin.setPosition(new Pose2D(DistanceUnit.MM,0,0, AngleUnit.RADIANS,0));
-        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        follower = Constants.createFollower(hardwareMap);
+        PanelsConfigurables.INSTANCE.refreshClass(this);
+        follower.startTeleopDrive();
+        follower.update();
+        follower.setStartingPose(new Pose(0,0,0));
+//        pin = hardwareMap.get(GoBildaPinpointDriver.class,"pinpoint");
+//        pin.setPosition(new Pose2D(DistanceUnit.MM,0,0, AngleUnit.RADIANS,0));
+//        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//
+//        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+//        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void teleDrive (double frontBackVelocity, double strafeVelocity, double turnVelocity){
-        double y = frontBackVelocity;
-        double x = strafeVelocity;
-        double rx = turnVelocity;
-        //follower.update();
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio, but only when
-        // at least one is out of the range [-1, 1]
-        double denominator = Math.max(abs(y) + abs(x) + abs(rx), 1);
-        double frontLeftPower = (y + x + rx) / denominator;
-        double backLeftPower = (y - x + rx) / denominator;
-        double frontRightPower = (y - x - rx) / denominator;
-        double backRightPower = (y + x - rx) / denominator;
-
-        frontLeftMotor.setPower(frontLeftPower);
-        frontRightMotor.setPower(frontRightPower);
-        backLeftMotor.setPower(backLeftPower);
-        backRightMotor.setPower(backRightPower);
-        pin.update();
+//        double y = frontBackVelocity;
+//        double x = strafeVelocity;
+//        double rx = turnVelocity;
+        follower.update();
+        follower.setTeleOpDrive(frontBackVelocity, -strafeVelocity, -turnVelocity, true);
+//        // Denominator is the largest motor power (absolute value) or 1
+//        // This ensures all the powers maintain the same ratio, but only when
+//        // at least one is out of the range [-1, 1]
+//        double denominator = Math.max(abs(y) + abs(x) + abs(rx), 1);
+//        double frontLeftPower = (y + x + rx) / denominator;
+//        double backLeftPower = (y - x + rx) / denominator;
+//        double frontRightPower = (y - x - rx) / denominator;
+//        double backRightPower = (y + x - rx) / denominator;
+//
+//        frontLeftMotor.setPower(frontLeftPower);
+//        frontRightMotor.setPower(frontRightPower);
+//        backLeftMotor.setPower(backLeftPower);
+//        backRightMotor.setPower(backRightPower);
+//        pin.update();
     }
     // 在 Drivetrain getter
-    public double getFrontLeftPower() {
-        return frontLeftMotor.getPower();
-    }
-
-    public double getFrontRightPower() {
-        return frontRightMotor.getPower();
-    }
-
-    public double getBackLeftPower() {
-        return backLeftMotor.getPower();
-    }
-
-    public double getBackRightPower() {
-        return backRightMotor.getPower();
-    }
+//    public double getFrontLeftPower() {
+//        return frontLeftMotor.getPower();
+//    }
+//
+//    public double getFrontRightPower() {
+//        return frontRightMotor.getPower();
+//    }
+//
+//    public double getBackLeftPower() {
+//        return backLeftMotor.getPower();
+//    }
+//
+//    public double getBackRightPower() {
+//        return backRightMotor.getPower();
+//    }
 
     public double getturretangle(){
-        double x = pin.getPosition().getX(DistanceUnit.MM)-aimPos.getX();
-        double y = pin.getPosition().getY(DistanceUnit.MM)-aimPos.getY();
-        double h = pin.getHeading(AngleUnit.RADIANS);
+        double x = follower.getPose().getX()-aimPos.getX();
+        double y = follower.getPose().getY()-aimPos.getY();
+        double h = follower.getHeading();
         if(y>0){
             return 1*h-Math.atan(abs(y)/abs(x));
         }
