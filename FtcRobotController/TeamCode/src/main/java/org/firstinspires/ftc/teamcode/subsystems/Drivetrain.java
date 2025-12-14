eb8f7b4f4d4643dced16fb0a548cc03012bc50cb
chenwen051116 <TavenChen@outlook.com>
2025-12-14 10:08:27 +0800

    Update Drivetrain.java

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -48,7 +48,7 @@ public class Drivetrain extends SubsystemBase {
     public Pose aimPos = bluenearAimPos;
 
     public Pose blueInitpose = new Pose(0.1224,-0.3717,3.141);
-    public Pose redInitpose = new Pose(-0.02583,-0.09087,3.141);
+    public Pose redInitpose = new Pose(-0.02583,-0.09087,-3.141);
 
     public static boolean TredFblue = false;
 
