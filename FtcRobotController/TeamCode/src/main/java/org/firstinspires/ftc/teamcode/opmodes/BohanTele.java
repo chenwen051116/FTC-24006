1eb312594c4ac67a296caae9074eb440bd30a851
chenwen051116 <TavenChen@outlook.com>
2026-01-13 19:08:28 -0800

    Update BohanTele.java

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BohanTele.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BohanTele.java
@@ -271,7 +271,8 @@ public class BohanTele extends CommandOpMode {
 ////        telemetry.addData("Shooterdis", shooter.distance);
 //        telemetry.addData("DRIVETRAIN GIVE ANGLE", drivetrain.getturretangle());
 //        telemetry.addData("turretpos", turret.getPos());
-//        telemetry.addData("turretaimpos", turret.aimposition);
+        telemetry.addData("turretaimangle", turret.aimangle);
+        telemetry.addData("turretcalc aim", turret.calculateAim());
 ////        telemetry.addData("1_Right_Trig", gamepad1.right_trigger);
 ////        telemetry.addData("ShotterForce?", shooter.forceShooting);
        telemetry.addData("accel", drivetrain.getaccel());
