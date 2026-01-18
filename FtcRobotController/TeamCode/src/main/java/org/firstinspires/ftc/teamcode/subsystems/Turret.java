7ecc5f431570793cc0a7d0e153d3af1ac112aad1
chenwen051116 <TavenChen@outlook.com>
2026-01-17 19:40:22 -0800

    Revert "Update Turret.java"
    This reverts commit fded86c1da831a16a12102adc13093d3cd3947ba.

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
@@ -132,9 +132,7 @@ public class Turret extends SubsystemBase {
         if(turretMotor.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER){
             turretMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         }
-        aimangle = arcangle;
         turretpidController.setSetPoint(calculateAim());
-       
         aimposition = calculateAim();
         turretpidController.setPIDF(encoderkp,encoderki,encoderkd,encoderkf);
         output = turretpidController.calculate(readAngle());
@@ -197,7 +195,7 @@ public class Turret extends SubsystemBase {
         if(turretMotor.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER){
             turretMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         }
-        aimangle = 0;
+        aimangle = Math.PI;
         turretpidController.setSetPoint(calculateAim());
         aimposition = calculateAim();
         turretpidController.setPIDF(encoderkp,encoderki,encoderkd,encoderkf);
