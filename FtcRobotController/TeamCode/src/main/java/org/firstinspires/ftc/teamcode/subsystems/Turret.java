c7b409e779709a2c5eae566dfb581f42c1fddddd
chenwen051116 <TavenChen@outlook.com>
2026-01-28 21:31:31 -0800

    Update Turret.java

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Turret.java
@@ -218,8 +218,8 @@ public class Turret extends SubsystemBase {
 
     public void manuelCenter(){
         if(centeringDir){
-            if(turretMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER){
-                turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            if(turretMotor.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER){
+                turretMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             }
             turretMotor.setPower(centerVel);
             if(isCentered()){
@@ -232,8 +232,8 @@ public class Turret extends SubsystemBase {
             }
         }
         else{
-            if(turretMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER){
-                turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            if(turretMotor.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER){
+                turretMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             }
             turretMotor.setPower(-centerVel);
             if(isCentered()){
