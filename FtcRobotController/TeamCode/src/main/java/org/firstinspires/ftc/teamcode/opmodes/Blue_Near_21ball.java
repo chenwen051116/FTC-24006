2dbc73f228a2b4537cc782a6784e720e3da2622f
chenwen051116 <TavenChen@outlook.com>
2026-02-26 20:15:43 -0800

    Update Blue_Near_21ball.java

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue_Near_21ball.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue_Near_21ball.java
@@ -127,19 +127,29 @@ public class Blue_Near_21ball extends OpMode {
 
         Shootpath3 = simplePath(FinishGather3,ShootPose);
 
+//        GatePath1 = drive.follower.pathBuilder()
+////                .setTValueConstraint(0.95)
+//                .addPath(new BezierLine(ShootPose, GatePassby))
+//                .setLinearHeadingInterpolation(ShootPose.getHeading(), GatePassby.getHeading())
+//                .addPath(new BezierLine(GatePassby, GatePassby2))
+//                .setLinearHeadingInterpolation(GatePassby.getHeading(), GatePassby2.getHeading())
+
         GatePath1 = drive.follower.pathBuilder()
 //                .setTValueConstraint(0.95)
                 .addPath(new BezierLine(ShootPose, GatePassby))
                 .setLinearHeadingInterpolation(ShootPose.getHeading(), GatePassby.getHeading())
-                .addPath(new BezierLine(GatePassby, GatePassby2))
-                .setLinearHeadingInterpolation(GatePassby.getHeading(), GatePassby2.getHeading())
-
                 .build();
         GatePath2 = drive.follower.pathBuilder()
 //                .setTValueConstraint(0.997)
-                .addPath(new BezierLine(GatePassby2, GatePose))
-                .setLinearHeadingInterpolation(GatePassby2.getHeading(), GatePose.getHeading())
+                .addPath(new BezierLine(GatePassby, GatePose))
+                .setLinearHeadingInterpolation(GatePassby.getHeading(), GatePose.getHeading())
                 .build();
+
+//        GatePath2 = drive.follower.pathBuilder()
+////                .setTValueConstraint(0.997)
+//                .addPath(new BezierLine(GatePassby2, GatePose))
+//                .setLinearHeadingInterpolation(GatePassby2.getHeading(), GatePose.getHeading())
+//                .build();
 
        GateShoot =
                drive.follower.pathBuilder()
